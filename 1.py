802502ec0c080ce8b822de7452cd43488d7fb4d3
angeluuuhe <88395849+angeluuuhe@users.noreply.github.com>
2021-08-03 11:40:43 -0500

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,43 @@
+from machine import Pin
+import utime
+import _thread
+
+cero = Pin (15,Pin.OUT)
+uno = Pin (2,Pin.OUT)
+dos = Pin (4,Pin.OUT)
+tres = Pin (16,Pin.OUT)
+cuatro = Pin (17,Pin.OUT)
+cinco = Pin (5,Pin.OUT)
+seis = Pin (18,Pin.OUT)
+siete = Pin (19,Pin.OUT)
+
+leds = [cero, uno, dos, tres, cuatro, cinco, seis, siete]
+
+def derecha():
+    
+    while True:
+        
+        for i in leds[0:4:1]:
+            i.value(1)
+            utime.sleep_ms(10)
+            i.value(0)
+            utime.sleep_ms(10)
+        
+    
+_thread.start_new_thread(derecha, ())    
+
+def izquierda():
+     for i in leds[4:8:1]:
+        i.value(1)
+        utime.sleep_ms(200)
+        i.value(0)
+        utime.sleep_ms(200)
+
+ 
+
+while True:
+   
+    izquierda()
+    utime.sleep(0.1)
+    
+    
